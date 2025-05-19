24a973c6de259b4133d67c881e91e326dbbfd71a
viktorji0624 <109110824+viktorji0624@users.noreply.github.com>
2025-05-19 14:38:11 +0800

    edit

--- viktorji/src/app/layout.tsx
+++ viktorji/src/app/layout.tsx
@@ -3,16 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Viktor Ji Website",
   description: "Viktor Ji's Personal Website/Portfolio",
